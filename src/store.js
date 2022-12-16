2f3cd7bb35075835dc0a106cfbe5c4f4e8afdfe1
chituenguyen <tuenguyen553@gmail.com>
2022-12-16 20:07:21 +0700

    show open course

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from "./redux/authSlice"
+import courseSlice from './redux/courseSlice'
 import examSlice from './redux/examSlice'
 
 export const store = configureStore({
   reducer: {
     userLogin: authSlice,
-    answer : examSlice
+    answer : examSlice,
+    course:courseSlice
   },
 })
